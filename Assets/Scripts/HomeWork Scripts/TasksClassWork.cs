2f322deab62916ce69673324280c877c0a5532a7
Doronnoga <doronnoga@gmail.com>
2024-11-28 12:52:53 +0200

    midwork file loader

--- Assets/Scripts/HomeWork Scripts/TasksClassWork.cs
+++ Assets/Scripts/HomeWork Scripts/TasksClassWork.cs
@@ -1,12 +1,17 @@
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Slider = UnityEngine.UI.Slider;
 
 public class TasksClassWork : MonoBehaviour
 {
-    [SerializeField] int boilingTime = 4;
+    [SerializeField] int waitTime;
+    [SerializeField] string fileName;
+    [SerializeField] private int loadPart;
     [SerializeField] private Slider slider;
+    private List<Task<string>> taskList;
     private CancellationTokenSource _cancelToken = new CancellationTokenSource();
 
     public void OnClickCancel()
@@ -21,58 +26,68 @@ public class TasksClassWork : MonoBehaviour
         Debug.Log("Click Start");
         _cancelToken.Cancel();
         _cancelToken = new CancellationTokenSource(); 
-        MakeCoffee();
+        LoadAllFiles();
     }
 
-    private async Task UpdateSliderAsync(float boilingDuration, CancellationToken token)
+    private async Task UpdateSliderAsync(float waitTime, int loadTime, CancellationToken token)
     {
-        float elapsedTime = 0f;
+        float elapsedTime = loadTime;
 
-        while (elapsedTime < boilingDuration)
+        while (elapsedTime < waitTime)
         {
             if (token.IsCancellationRequested)
             {
-                Debug.Log("Boiling canceled.");
-                return; // Exit the task if canceled
+                Debug.Log("Loading canceled.");
+                return;
             }
 
             elapsedTime += Time.deltaTime;
-            float progress = Mathf.Clamp01(elapsedTime / boilingDuration); // Normalize progress
+            float progress = Mathf.Clamp01(elapsedTime / waitTime); 
             slider.value = progress;
 
-            await Task.Yield(); // This ensures the task yields control to the main thread
+            await Task.Yield();
 
         }
         slider.value = 1f;
     }
-    private async Task<string> BoilWater(int boilingTime , CancellationToken token)
+    private async Task<string> Fileloader(int waitTime , string fileName, int loadPart, CancellationToken token)
     {
-        Debug.Log("Filled kettle with water.");
-        Debug.Log("Turned the kettle on.");
-
+        Debug.Log($"started loading file with name {fileName}.");
         try
         {
-            Debug.Log("The water is being boiled, please wait...");
-            await Task.Delay(boilingTime * 1000, token);
-            return "The kettle is done, the water is boiling.";
+            Debug.Log($"{fileName}, is loading please wait...");
+            await Task.Delay(waitTime * 1000, token);
+            return $"{fileName} is done loading.";
         }
         
         catch (TaskCanceledException)
         {
-            Debug.Log("Boiling canceled.");
-            return "The kettle has turned off.";
+            Debug.Log("Loading canceled.");
+            return "The Loading has been turned off.";
         }
     }
 
-    private async void MakeCoffee()
+    private void CycleThroughList()
     {
-        Debug.Log("Let's make coffee.");
+        taskList = new List<Task<string>>
+        {
+            Fileloader(3, "MyFile", loadPart, _cancelToken.Token),
+            Fileloader(4, "DoNotOpen", loadPart, _cancelToken.Token),
+            Fileloader(5, "GroceryList", loadPart, _cancelToken.Token),
+            Fileloader(2, "PrivateBussinessFile", loadPart, _cancelToken.Token),
+            Fileloader(1, "NOtes", loadPart, _cancelToken.Token),
+        };
+    }
 
-        var boilingTask = BoilWater(boilingTime , _cancelToken.Token);
-        var sliderTask = UpdateSliderAsync(boilingTime, _cancelToken.Token);
+    private async void LoadAllFiles()
+    {
+        Debug.Log("Let's load all files:");
+
+        var boilingTask = Fileloader(waitTime ,"PrivateFile", 1, _cancelToken.Token);
+        var sliderTask = UpdateSliderAsync(waitTime, loadPart, _cancelToken.Token);
         
         await Task.WhenAny(boilingTask, sliderTask);
-        Debug.Log("Drink coffee, enjoy async tasks.");
+        Debug.Log("All Files have been loaded.");
     }
   
 }
